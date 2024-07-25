783206a8e9be3fa43278c91a714951f9e7e30d08
Ansh Jindal <jindalansh498@gmail.com>
2024-07-25 09:12:46 -0400

    updated hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -44,7 +44,7 @@ function Hero() {
                   cursor={true}
                   sequence={[
                     500,
-                    'A Full-Stack Developer.',
+                    'A Frontend Developer.',
                     1000,
                     'A problem solver.',
                     1000,
